322b7ee17e1a337b4cb7ae1ae8f328d4402258bd
Andreas Daxer <info@andreas-daxer.de>
2023-11-27 12:35:33 +0100

    Liste mit Paging, einzelnes Movie

--- MovieBase.Api/Controllers/MoviesController.cs
+++ MovieBase.Api/Controllers/MoviesController.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using MovieBase.Common;
 using MovieBase.Data;
+using System.Net.Mime;
 
 namespace MovieBase.Api.Controllers;
 [ApiController]
@@ -16,9 +18,40 @@ public class MoviesController : ControllerBase
         _context = context;
     }
 
-    [HttpGet("[action]", Name = "MovieList")]
-    public IEnumerable<Movie> List()
+    [HttpGet("[action]/{pageSize}/{pageNo}", Name = "MovieList")]
+    [Produces(MediaTypeNames.Application.Json, MediaTypeNames.Application.Xml)]
+    public async Task<IActionResult> List(int pageSize, int pageNo)
     {
-        return _context.Movies.Take(10).ToArray();
+        var data = await _context.Movies.Skip(pageNo*pageSize).Take(pageSize).ToArrayAsync();
+        return Ok(data);
     }
+
+    [HttpGet("{id}", Name = "Movie")]
+    [Produces(MediaTypeNames.Application.Json, MediaTypeNames.Application.Xml)]
+    public async Task<IActionResult> One(int id)
+    {
+        var data = await _context.Movies.FindAsync(id);
+        return data==null
+            ? NotFound()
+            : Ok(data);
+    }
+    //public async Task<IActionResult> One(string id)
+    //{
+    //    if (int.TryParse(id, out var realId))
+    //    {
+    //        var data = await _context.Movies.FindAsync(realId);
+    //        return data == null
+    //            ? NotFound()
+    //            : Ok(data);
+    //    }
+    //    return BadRequest("An int is expected");
+    //}
+
+}
+
+public class Result<T>
+{
+    public bool Succeeded { get; set; }
+    public T? Payload { get; set; }
+    public string? Message { get; set; }
 }
--- MovieBase.Api/Program.cs
+++ MovieBase.Api/Program.cs
@@ -12,7 +12,8 @@ public class Program
 
         // Add services to the container.
 
-        builder.Services.AddControllers();
+        builder.Services.AddControllers().AddXmlSerializerFormatters();
+
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
